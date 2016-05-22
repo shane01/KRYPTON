9837d4d7298643a2e3f47395a5a496fbea6b8030
shane01 <strainedhalo@gmail.com>
2016-05-22 15:33:54 +0400

    Readd D724 support

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 export ARCH=arm
 export CROSS_COMPILE=~/Android/KRYPTON/arm-eabi-4.9-sm/bin/arm-eabi-
+echo "Enter phone model"
+read device
 echo "Clean directories? y/n"
 read instruct
 if [ "$instruct" = "y" ]
 then
 echo "Wait" 
 make clean 
-echo "Done"
-fi
 
 if [ -e "./arch/arm/boot/dt.img" ]; then
 rm ./arch/arm/boot/dt.img
@@ -17,13 +17,28 @@ if [ -e "./arch/arm/boot/msm8226-v1-jagnm.dtb" ]; then
 rm ./arch/arm/boot/msm8226-v1-jagnm.dtb
 rm ./arch/arm/boot/msm8226-v2-jagnm.dtb
 fi
+if [ -e "./arch/arm/boot/msm8226-jag3gds.dtb" ]; then
+rm ./arch/arm/boot/msm8226-jag3gds.dtb
+fi
+echo "Done"
+fi
 
 echo "Make new defconfig? y/n" 
 read defconfig
 if [ "$defconfig" = "y" ]
 then
+
+if [ "$device" = "D722" ]
+then 
 ARCH=arm CROSS_COMPILE=~/arm-eabi-4.9-sm/bin/arm-eabi- make jagnm_cyanogenmod_defconfig
 fi
+
+if [ "$device" = "D724" ]
+then 
+ARCH=arm CROSS_COMPILE=~/arm-eabi-4.9-sm/bin/arm-eabi- make jag3gds_cyanogenmod_defconfig
+fi
+fi
+
 
 echo "Configure kernel? y/n"
 read config
@@ -46,7 +61,7 @@ then
 ./dtbToolCM -2 -s 2048 -p ./scripts/dtc/ -o ./arch/arm/boot/dt.img ./arch/arm/boot/
 echo "Copying files to respective folder"
 
-		cd ./RAMDISK/D722/
+		cd ./RAMDISK/$device/
 		./cleanup.sh
 		./unpackimg.sh boot.img
 		cp ../boot.img-ramdiskcomp ./split_img/boot.img-ramdiskcomp
@@ -59,23 +74,21 @@ echo "Copying files to respective folder"
 		./bump.py image-new.img
                 cd ../../
 		echo "Moving Kernel to output folder"
-		mv ./RAMDISK/D722/image-new_bumped.img ./Output/D722.img
+		mv ./RAMDISK/$device/image-new_bumped.img ./Output/$device.img
 
 
 
 echo "Copying image to root of unzipped directory renaming it boot."
 
-        cp ./Output/D722.img ./Output/KRYPTON/boot.img
+        cp ./Output/$device.img ./Output/KRYPTON/boot.img
 	
 echo "Changing the directory to root of KRYPTON directory."
     cd ./Output/KRYPTON
 echo "Creating flashable zip."
-zip -r KRYPTONKernel-$(date +%F).zip . -x ".*"
+zip -r KRYPTONKernel$device-$(date +%F).zip . -x ".*"
 
     echo "Moving zipped file to output folder."
 
-    mv *.zip  ../../Release
+    mv *.zip  ../../Release/$device
 echo " Kernel is found at Release folder"
 fi
-
-
